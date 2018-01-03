aecbfd671c24789900a8430e3432b0e5338682b8
Igor Polevoy <igor@expresspigeon.com>
2018-01-02 22:36:58 -0600

    work in progress

---
+++ activeweb-testing/src/main/java/org/javalite/activeweb/annotations/IncludeFilters.java
@@ -0,0 +1,18 @@
+package org.javalite.activeweb.annotations;
+
+import org.javalite.activeweb.AppSpec;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Add annotation to the {@link AppSpec} decedents to avoid opening a DB connection for tests.
+ *
+ * @author igor on 12/9/17.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface IncludeFilters {}
+
---
+++ activeweb-testing/src/test/java/app/controllers/app_spec/IncludeFiltersSpec.java
@@ -0,0 +1,17 @@
+package app.controllers.app_spec;
+
+import org.javalite.activeweb.AppSpec;
+import org.junit.Test;
+
+/**
+ * @author igor on 12/16/17.
+ */
+public class IncludeFiltersSpec extends AppSpec {
+
+    @Test
+    public void shouldIncludeFilters(){
+
+
+
+    }
+}
